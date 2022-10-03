3bfd14cf62b0728e009542c5b378d31092257027
anshuklaGL <98663062+anshuklaGL@users.noreply.github.com>
2022-10-03 23:47:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const api = {
+    key: "7e3f21edee540e6110af347b55eb1ab2",
+    base: "https://api.openweathermap.org/data/2.5/",
+    units: "metric"
+  }
+
+
+
+
+const searchbox=document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt){
+    if(evt.keyCode == 13){
+        getResults(searchbox.value);
+    }
+}
+
+function getResults(){
+    
+}
